0a8ee391504083d477265929c4f30f3499ec814b
Sarvesh Yadav <127607339+sarveshpyadav@users.noreply.github.com>
2024-01-04 18:36:17 +0530

    Fix a rollup error
    Reference proper file name in "Hone.jsx"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Favorites from "../components/Favorites";
-import Trending from "../components/trending";
+import Trending from "../components/Trending";
 import Banner from "../components/Banner";
 
 function Home() {
